fe964ebc9a1ce2d5c9303a31abb45a14f7be6adb
Dawid Nowotny <dawidnowotny@o2.pl>
2024-10-08 12:43:39 +0200

    Added endpoint for logging in

--- backend/src/user/router.py
+++ backend/src/user/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, status
 
 from .schemas import UserCreate, UserLogin
-from .service import register_user
+from .service import register_user, login_user
 from database import get_db_connection
 
 router = APIRouter()
@@ -13,3 +13,13 @@ async def register(user: UserCreate, conn = Depends(get_db_connection)):
     return {
         "message": "Użytkownik pomyślnie zarejestrowany."
     }
+
+@router.post("/login", status_code=status.HTTP_200_OK)
+async def login(user: UserLogin, conn = Depends(get_db_connection)):
+    id, username, email = await login_user(user, conn)
+
+    return {
+        "id": id,
+        "username": username,
+        "email": email
+        }
--- backend/src/user/service.py
+++ backend/src/user/service.py
@@ -2,7 +2,7 @@ import asyncpg
 from fastapi import HTTPException, status
 from passlib.context import CryptContext
 
-from .schemas import UserCreate
+from .schemas import UserCreate, UserLogin
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -20,15 +20,14 @@ async def check_user_exists(user_data: UserCreate, conn: asyncpg.connection.Conn
     if user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Tala nazwa użytkownika lub email juz istnieje!"
+            detail="Taka nazwa użytkownika lub email juz istnieje!"
         )
 
 async def register_user(user_data: UserCreate, conn: asyncpg.connection.Connection) -> None:
     hashed_password = hash_password(user_data.password)
     await check_user_exists(user_data, conn)
-
     query = f"INSERT INTO users (username, email, password) VALUES ('{user_data.username}', '{user_data.email}', '{hashed_password}')"
-    
+        
     try:
         await conn.execute(query)
     except Exception as e:
@@ -36,3 +35,16 @@ async def register_user(user_data: UserCreate, conn: asyncpg.connection.Connecti
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Nieoczekiwany błąd serwera: {str(e)}"
         )
+
+async def login_user(user_data: UserLogin, conn: asyncpg.connection.Connection) -> tuple[str, str, str]:
+    query = f"SELECT * FROM users WHERE (username = '{user_data.username}' OR email = '{user_data.username}')"
+
+    user = await conn.fetchrow(query)
+
+    if not user or not verify_password(user_data.password, user['password']):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Nieprawidłowe dane logowania!"
+        )
+    
+    return user['id'], user['username'], user['email']
